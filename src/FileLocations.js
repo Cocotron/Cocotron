d313641ef2aa9c4e27937ca11ac562b4d18756b1
cbruno <cbruno@hokuasoftware.com>
2022-12-28 08:43:28 -0600

    release framework

--- src/FileLocations.js
+++ src/FileLocations.js
@@ -8,7 +8,7 @@ const FileLocations = {
   BUILD_DIR: path.resolve(rootPath + "/public/build"),
   BUILD_DIR_DEBUG: path.resolve(rootPath + "/public/build/Debug"),
   PROCESSED_SOURCE: path.resolve(rootPath + "/public/build/Debug/main.j"),
-  BUILD_DIR_RELEASE: path.resolve(rootPath + "public/build/Release"),
+  BUILD_DIR_RELEASE: path.resolve(rootPath + "/public/build/Release"),
   SRC_DIR: path.resolve(rootPath + "/src"),
   FRAMEWORKS_DIR: path.resolve(rootPath + "/Frameworks"),
   PROJECT_PACKAGE: path.resolve(rootPath + "/package.json"),
--- src/bin/Cocotron.js
+++ src/bin/Cocotron.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const path = require("path");
 const FileLocations = require("../FileLocations.js");
 
 const { buildRelease } = require("../ProjectBuilder/index.js");
@@ -15,12 +16,16 @@ if (args.length > 1) {
     if (args.length > 2) {
       const projectName = args[2];
       const createProject = require("../ProjectCreator");
-
       createProject(projectName, args.includes("--framework"));
     }
   } else if (cmd === "release") {
     if (fs.existsSync(FileLocations.PROJECT_PACKAGE)) {
       const PACKAGE = require(FileLocations.PROJECT_PACKAGE);
+      if(PACKAGE.isFramework) {
+        fs.cpSync(FileLocations.SRC_DIR, path.join(FileLocations.FRAMEWORKS_DIR, PACKAGE.name), {recursive: true});
+        fs.cpSync(FileLocations.PROJECT_PACKAGE, path.join(FileLocations.FRAMEWORKS_DIR, PACKAGE.name, "package.json"));
+        return;
+      }
       if (!PACKAGE.main) {
         throw new Error(`"main" file is undefined in package.json.`);
       }
